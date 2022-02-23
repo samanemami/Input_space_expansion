85109893021e205b3b2e1689cf78ac133e51c48f
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2022-02-23 19:45:44 +0100

    update

--- erc.py
+++ erc.py
@@ -1,12 +1,20 @@
 # %%
 from pyexpat import model
+from matplotlib.pyplot import axis
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, KFold
 import pandas as pd
 import numpy as np
 from sklearn import datasets as dts
 
-X, y = dts.make_regression(n_samples=500, n_features=5, n_targets=3)
+
+def input(X, y, i):
+    X = np.append(X, y[:, i][:, np.newaxis], axis=1)
+    return X
+
+
+m = 3
+X, y = dts.make_regression(n_samples=500, n_features=5, n_targets=m)
 
 
 kfold = KFold(n_splits=2, shuffle=True)
@@ -16,10 +24,18 @@ score_x_t1 = []
 score_x_t2 = []
 score_x_t3 = []
 
-score_xy_t1 =[]
-score_xy1_t2 =[]
-score_xy1_t3 =[]
 
+score_xy1_t2 = []
+score_xy1_t3 = []
+
+score_xy_t1 = []
+score_xy_t1 = []
+
+score_xy2_t3 = []
+score_xy2_t1 = []
+
+score_xy3_t2 = []
+score_xy3_t1 = []
 
 
 for i, (train_index, test_index) in enumerate(kfold.split(X, y)):
@@ -39,14 +55,8 @@ for i, (train_index, test_index) in enumerate(kfold.split(X, y)):
     # train the model by considering the output as an input
     # Add the first output
 
-    x_train = np.append(x_train, y_train[:, 0][:, np.newaxis], axis=1)
-    model.fit(x_train, y_train[:, 1])
-    score_xy1_t2.append(model.score(x_test, y_test[:, 1]))
-    model.fit(x_train, y_train[:, 2])
-    score_xy1_t3.append(model.score(x_test, y_test[:, 2]))
+    for i in range(m):
+        x_train = input(x_train, y_train, i)
+        x_test = input(x_train, y_train, i)
 
-    
-# %%
-X = np.append(x_train, y_train[:, 0][:, np.newaxis], axis=1)
-# y_train[:, 0][:, np.newaxis].shape
-# x_train.shape
+        model.fit(x_train, y_train[:, ])
