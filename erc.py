da40a9553b9dfa6977314bef642100f1964854ec
Seyedsaman (sam) Emami <saman.emami@hotmail.com>
2022-02-24 15:35:12 +0100

    update

--- erc.py
+++ erc.py
@@ -18,8 +18,8 @@ X, y = dts.make_regression(n_samples=500, n_features=5, n_targets=m)
 
 
 kfold = KFold(n_splits=cv, shuffle=True)
-
-scores = np.zeros((2, ((m-1) * m)))
+n_targest = ((m-1) * m) + y.shape[1]
+scores = np.zeros((cv, n_targest))
 scores = pd.DataFrame(scores, columns=pd.RangeIndex(0, scores.shape[1], 1))
 
 for cv_, (train_index, test_index) in enumerate(kfold.split(X, y)):
@@ -36,17 +36,27 @@ for cv_, (train_index, test_index) in enumerate(kfold.split(X, y)):
         scores.iloc[cv_, i] = score
         mapping = {scores.columns[i]: 'target_'+str(i)}
         scores = scores.rename(columns=mapping)
+
     # train the model by considering the output as
     # an input
 
-    # for i in range(0, y_train.shape[1], 1):
-    #     x_train = input(x_train, y_train, i)
-    #     x_test = input(x_test, y_test, i)
-    #     for j in range(0, y_train.shape[1], 1):
-    #         if i != j:
-    #             model.fit(x_train, y_train[:, j])
-    #             score = model.score(x_test, y_test[:, j])
-    #             scores.iloc[cv_, i+y.shape[1]] = score
+    col=False
+    for i in range(0, y_train.shape[1], 1):
+        x_train = input(x_train, y_train, i)
+        x_test = input(x_test, y_test, i)
+        print('--------')
+        if col:
+            col += 1    
+        else:
+            col = i+y.shape[1]
+        for j in range(0, y_train.shape[1], 1):
+            if i != j:
+                # model.fit(x_train, y_train[:, j])
+                # score = model.score(x_test, y_test[:, j])
 
+                print(col)
+                # scores.iloc[cv_, col] = score
+                # mapping = {scores.columns[i+y.shape[1]]: 'X+target_'+str(i) + '|target_'+str(j)}
+                # scores = scores.rename(columns=mapping)
+                col += 1
 
-# %%
